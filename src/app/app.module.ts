ff730d91afac100f48a7cf9faff0ddc7e0ed6648
Alteo Caka <alteo.caka@gmail.com>
2020-07-11 16:45:25 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { FieldComponent } from './field/field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule,
