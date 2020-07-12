9b19e05fef7d49f9da30e104b1fe3258956d3678
Alteo Caka <alteo.caka@gmail.com>
2020-07-12 11:08:31 +0200

    Updated UI + small refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Routing:
 import { AppRoutingModule } from './app-routing.module';
+
+// Components:
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { FieldComponent } from './field/field.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Angular material:
-
 import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
